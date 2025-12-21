7b6bc038b9c2c839b3805a8c6031bcd8790d8e38
Adithya Athreya <adithyaathreya2005@gmail.com>
2025-12-21 12:59:18 +0530

    removed unused imports and unused dataDir field

--- modules/storage-engine/src/main/java/com/dkv/storage/CompactionManager.java
+++ modules/storage-engine/src/main/java/com/dkv/storage/CompactionManager.java
@@ -3,10 +3,6 @@ package com.dkv.storage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -15,12 +11,10 @@ import java.util.concurrent.TimeUnit;
 public class CompactionManager {
     private static final Logger logger = LoggerFactory.getLogger(CompactionManager.class);
     private final LSMStorageEngine engine;
-    private final String dataDir;
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     public CompactionManager(LSMStorageEngine engine, String dataDir) {
         this.engine = engine;
-        this.dataDir = dataDir;
     }
 
     public void start() {
@@ -47,7 +41,7 @@ public class CompactionManager {
         // Optimization: Use K-Way Merge with iterators (Mocking this with simple
         // approach first)
 
-        // TODO: Implement actual merge sort of SSTables
+        
         // For now, let's just log.
         // Implementing full K-way merge is complex for this step size.
         // I will leave it as a placeholder or implement a very basic "Minor
